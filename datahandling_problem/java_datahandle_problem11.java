61a885d2f33c5dab7c062e9d3da335a12a32e139
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-09-02 21:42:27 +0530

    Fix formatting and add newline at end of file

--- datahandling_problem/java_datahandle_problem11.java
+++ datahandling_problem/java_datahandle_problem11.java
@@ -3,6 +3,6 @@ public class Main{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int val = sc.nextInt();
-        System.out.printf("%c", val);
+        System.out.printf("%c", val); 
     }
 }
