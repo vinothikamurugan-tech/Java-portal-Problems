f96d01199cd6700c545a1e69ecc8c5c5c63a7996
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-09-02 21:42:49 +0530

    Update java_datahandle_problem12.java

--- datahandling_problem/java_datahandle_problem12.java
+++ datahandling_problem/java_datahandle_problem12.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Main{
-    public static void main(String args[]){
+    public static void main(String args[]){ 
         Scanner sc = new Scanner(System.in);
         char ch = sc.next().charAt(0);
         System.out.println(ch);
