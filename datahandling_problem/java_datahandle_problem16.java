97718bdef8035a870d70dee2d91ddf0345e6f6ab
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-09-19 16:14:23 +0530

    Add files via upload

---
+++ datahandling_problem/java_datahandle_problem16.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float f = sc.nextFloat();
+        System.out.printf("%.2f", f);
+    }
+}
